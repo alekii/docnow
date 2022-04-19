1cb63cd5529651fc6b461726c455b2fcf6fe635d
alekii <lennoxxbrownn@gmail.com>
2022-04-19 13:09:23 +0300

    switched to spring-data-rest

--- src/main/java/com/docnow/docnow/entity/Doctor.java
+++ src/main/java/com/docnow/docnow/entity/Doctor.java
@@ -1,6 +1,12 @@
 package com.docnow.docnow.entity;
 
 import javax.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 
  @Entity
 @Table(name="doctors")
@@ -21,65 +27,5 @@ public class Doctor {
 
     @Column(name="hospital")
       private String hospital;
-
-  public Doctor(){
-
-  }
-    public Doctor(String name, int age, String speciality, String hospital) {
-        this.name = name;
-        this.age = age;
-        this.speciality = speciality;
-        this.hospital = hospital;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getSpeciality() {
-        return speciality;
-    }
-
-    public String getHospital() {
-        return hospital;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setSpeciality(String speciality) {
-        this.speciality = speciality;
-    }
-
-    public void setHospital(String hospital) {
-        this.hospital = hospital;
-    }
-
-     public int getId() {
-         return id;
-     }
-
-     public void setId(int id) {
-         this.id = id;
-     }
-
-     @Override
-    public String toString() {
-        return "Doctor{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", speciality='" + speciality + '\'' +
-                ", hospital='" + hospital + '\'' +
-                '}';
-    }
 
  }
